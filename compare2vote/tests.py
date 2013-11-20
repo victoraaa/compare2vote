7fbf20bc1a936f3e32f285666487245818f0f4bc
Victor <victoraaa@gmail.com>
2013-11-20 00:20:33 -0200

    Real test added

--- compare2vote/tests.py
+++ compare2vote/tests.py
@@ -4,17 +4,14 @@ import poll_models
 
 class TestPollModel(unittest.TestCase):
 
-    def test_add_option(self):
-        poll = poll_models.Poll("title", "question")
-        self.assertEquals("title", poll.title)
-
-        # should raise an exception for an immutable sequence
-        #self.assertRaises(TypeError, random.shuffle, (1,2,3))
-
     def test_cant_add_two_options_with_same_name(self):
-        #element = random.choice(self.seq)
-        #self.assertTrue(element in self.seq)
-        pass
+        poll = poll_models.Poll("title", "question")
+        option = poll_models.PollOption("name", "image_url")
+        poll.add_option(option)
+        self.assertEquals(option, poll.get_option_by_name("name"))
+
+        with self.assertRaises(Exception):
+            poll.add_option(poll_models.PollOption("name", "image2"))
 
 
 if __name__ == '__main__':
